fdead2cde8cd3d936d9b175db1b74ec192fe16ad
Nazwa Septya Salsabilla <nazwasalsabila1002@gmail.com>
2023-05-06 20:47:05 +0700

    coba checkout

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,11 +1,11 @@
 const api = 'https://64510c9ca3221969115946ad.mockapi.io'
 let users = []
-const a = (async () => {
+const a = (async() => {
     const data = await logJSONData();
     users.push(data);
 })();
 const form = document.getElementById("form");
-form.addEventListener("submit", function (event) {
+form.addEventListener("submit", function(event) {
     event.preventDefault()
     handleGetFormData();
 });
@@ -21,16 +21,17 @@ function handleGetFormData() {
     if (!isEmailExist(email) || !isPasswordExist(password)) {
         alert('email atau password salah');
         return false;
-    }
-    else {
+    } else {
         setCookie("userLogin", findByEmail(email));
         window.location.href = "/dashboard.html";
     }
 
 }
+
 function isPasswordExist(password) {
     return users[0].some(user => user.password === password);
 }
+
 function isEmailExist(email) {
     return users[0].some(user => user.email === email);
 }
@@ -38,6 +39,7 @@ function isEmailExist(email) {
 function findByEmail(email) {
     return users[0].find(user => user.email === email);
 }
+
 function setCookie(name, value) {
     const date = new Date();
     date.setTime(date.getTime() + (24 * 60 * 60 * 1000)); // set the expiration time to 1 day
@@ -50,4 +52,3 @@ async function logJSONData() {
     const jsonData = await response.json();
     return jsonData
 }
-
